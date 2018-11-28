b00adacc518a4a272e5d2b978cd9a04e76009085
Danny Teng <tengdann@umich.edu>
2018-11-28 11:04:11 -0500

    Finished up to EC1

--- top_headlines.py
+++ top_headlines.py
@@ -1,10 +1,31 @@
 from flask import Flask, render_template
 import requests
+import secrets
+import json
 app = Flask(__name__)
+
+api_key = secrets.api_key
+baseurl = 'https://api.nytimes.com/svc/topstories/v2/'
+
+def make_url(section, format):
+    url = baseurl
+    url += section + '.' + format
+    url += '?api_key=' + api_key
+    return url
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/user/<usr>')
 def user(usr):
-    return render_template('user.html', name = usr)
+    readback = json.loads(requests.get(make_url('technology', 'json')).text)
+    return render_template('user.html', name = usr, topic = 'technology', my_list = readback['results'][:5])
+    
+@app.route('/user/<usr>/<tpc>')
+def topic(usr, tpc):
+    readback = json.loads(requests.get(make_url(str(tpc), 'json')).text)
+    return render_template('user.html', name = usr, topic = tpc, my_list = readback['results'][:5])
     
     
 if __name__ == '__main__':
