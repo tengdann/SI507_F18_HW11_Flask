364183174b69ef9c0c25a041bc4f2276a5501852
Danny Teng <tengdann@umich.edu>
2018-11-28 16:26:56 -0500

    Fixed EC1

--- top_headlines.py
+++ top_headlines.py
@@ -17,15 +17,16 @@ def make_url(section, format):
 def index():
     return render_template('index.html')
 
+page_list = ['home', 'opinion', 'world', 'national', 'politics', 'upshot', 'nyregion', 'business', 'technology', 'science', 'health', 'sports', 'arts', 'books', 'movies', 'theatre', 'sundayreview', 'fashion', 'tmagazine', 'food', 'travel', 'magazine', 'realestate', 'automobiles', 'obituaries', 'insider']
 @app.route('/user/<usr>')
 def user(usr):
     readback = json.loads(requests.get(make_url('technology', 'json')).text)
-    return render_template('user.html', name = usr, topic = 'technology', my_list = readback['results'][:5])
+    return render_template('user.html', name = usr, topic = 'technology', my_list = readback['results'][:5], list_page = page_list)
     
 @app.route('/user/<usr>/<tpc>')
 def topic(usr, tpc):
     readback = json.loads(requests.get(make_url(str(tpc), 'json')).text)
-    return render_template('user.html', name = usr, topic = tpc, my_list = readback['results'][:5])
+    return render_template('topic.html', name = usr, topic = tpc, my_list = readback['results'][:5])
     
     
 if __name__ == '__main__':
